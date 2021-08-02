08ac7ae9b81c0f94bf662a3def9032b551bd52c7
Argen <aisakunov22@gmail.com>
2021-08-02 19:57:29 +0300

    release1

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const buttonStart = document.querySelector('[data-action="start"]');
+const buttonStop = document.querySelector('[data-action="stop"]');
+
 const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
 
 let ranTimes = null;
@@ -6,15 +9,16 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-document.querySelector('[data-action="start"]').addEventListener('click', () => {
+buttonStart.addEventListener('click', () => {
   ranTimes = setInterval(() => {
     const random = randomIntegerFromInterval(0, colors.length - 1);
 
     document.body.style.backgroundColor = colors[random];
   }, 1000);
+  buttonStart.disabled = true;
 });
 
-document.querySelector('[data-action="stop"]').addEventListener('click', () => {
+buttonStop.addEventListener('click', () => {
   clearInterval(ranTimes);
+  buttonStart.disabled = false;
 });
-
